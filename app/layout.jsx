bbb9f5530f348cd5a1279d87c743a5eb70fd7584
Dawa Zangpo <32727869+zangpodawa@users.noreply.github.com>
2024-06-26 09:22:25 +1000

    Updated layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,8 +4,8 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & Share AI Prompts",
+  title: "Socodetes",
+  description: "Discover & Share Philosopher codes",
 };
 
 const RootLayout = ({ children }) => (
